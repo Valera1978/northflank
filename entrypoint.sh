6c85ae967d8f1372281e3d10b1363be53b8595fd
bonuzzz <6509127+bonuzzz@users.noreply.github.com>
2024-08-18 18:18:25 +0300

    dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
-
-sed -i "s#UUID#$UUID#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
-sed -i "s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
-sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
-
 ### nginx
 rm -rf /usr/share/nginx/*
 wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
@@ -13,15 +7,14 @@ unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
 rm -f /usr/share/nginx/mikutap.zip
 
 ### xray
-
+RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mv xray ${RELEASE_RANDOMNESS}
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
-#cat config.json | base64 > config
-#rm -f config.json
 
-#nginx
-#base64 -d config > config.json
-#./${RELEASE_RANDOMNESS} -config=config.json
+## config
+sed -i "s#UUID#$UUID#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
+sed -i "s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
+sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
